ee6077626cbed015dc59330d41b4a77bd01e584a
BrockBaron <Nslekkas@gmail.com>
2021-05-28 20:45:59 +0100

    Cable test completed

--- src/test/java/EquipmentTest/CableTest.java
+++ src/test/java/EquipmentTest/CableTest.java
@@ -4,16 +4,57 @@ import Equipment.Cable;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class CableTest {
 
     Cable cable;
 
     @Before
     public void setUp(){
-        cable = new Cable("Dragon BiWire", "AudioQuest", "Cable", "Speaker cable", 150.50, 66800.99, 10, 4);
+        cable = new Cable("Dragon BiWire", "AudioQuest", "Cable", "Speaker cable", 150.50, 66800.99, 10, 4.0);
     }
 
     @Test
-    public void name() {
+    public void hasName() {
+        assertEquals("Dragon BiWire", cable.getEquipmentName());
+    }
+
+    @Test
+    public void hasBrand() {
+        assertEquals("AudioQuest", cable.getEquipmentBrand());
+    }
+
+    @Test
+    public void hasType() {
+        assertEquals("Cable", cable.getEquipmentType());
+    }
+
+    @Test
+    public void hasSubtype() {
+        assertEquals("Speaker cable", cable.getEquipmentSubType());
+    }
+
+
+    @Test
+    public void getWholesalePrice() {
+        assertEquals(150.50, cable.getWholesalePrice(), 0.1);
+    }
+
+
+    @Test
+    public void getRetailPrice() {
+        assertEquals(66800.99, cable.getRetailPrice(), 0.1);
+
+    }
+
+    @Test
+    public void hasGaugeValue() {
+        assertEquals(10, cable.getWireGauge());
+    }
+
+    @Test
+    public void hasLength() {
+        assertEquals(4, cable.getWireLenght(), 0.2);
     }
 }
