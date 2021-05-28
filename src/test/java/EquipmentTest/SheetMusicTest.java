ce00cb907e3f5dda3aa54e2612b4ba50ddafaa5c
BrockBaron <Nslekkas@gmail.com>
2021-05-28 20:50:41 +0100

    Sheet music tsts start

--- src/test/java/EquipmentTest/SheetMusicTest.java
+++ src/test/java/EquipmentTest/SheetMusicTest.java
@@ -1,4 +1,22 @@
 package EquipmentTest;
 
+import Equipment.SheetMusic;
+import org.junit.Before;
+import org.junit.Test;
+
 public class SheetMusicTest {
+
+    SheetMusic sheet;
+
+    @Before
+    public void setUp(){
+        sheet = new SheetMusic("Easy Solos", "Hal Leonard", "Sheet Music", "Jazz", 1.50, 12.50, 16, "Kenny G");
+    }
+
+
+    @Test
+    public void name() {
+    }
 }
+
+
