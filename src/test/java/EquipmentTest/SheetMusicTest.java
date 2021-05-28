a709171e1f12b5429b73e62338012b1497f17d79
BrockBaron <Nslekkas@gmail.com>
2021-05-28 20:53:09 +0100

    Sheet music all test passed

--- src/test/java/EquipmentTest/SheetMusicTest.java
+++ src/test/java/EquipmentTest/SheetMusicTest.java
@@ -4,6 +4,8 @@ import Equipment.SheetMusic;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class SheetMusicTest {
 
     SheetMusic sheet;
@@ -15,7 +17,46 @@ public class SheetMusicTest {
 
 
     @Test
-    public void name() {
+    public void hasName() {
+        assertEquals("Easy Solos", sheet.getEquipmentName());
+    }
+
+    @Test
+    public void hasBrand() {
+        assertEquals("Hal Leonard", sheet.getEquipmentBrand());
+    }
+
+    @Test
+    public void hasType() {
+        assertEquals("Sheet Music", sheet.getEquipmentType());
+    }
+
+    @Test
+    public void hasSubtype() {
+        assertEquals("Jazz", sheet.getEquipmentSubType());
+    }
+
+
+    @Test
+    public void getWholesalePrice() {
+        assertEquals(1.50, sheet.getWholesalePrice(), 0.1);
+    }
+
+
+    @Test
+    public void getRetailPrice() {
+        assertEquals(12.50, sheet.getRetailPrice(), 0.1);
+
+    }
+
+    @Test
+    public void getNoOfPages() {
+        assertEquals(16, sheet.getNoOfPages());
+    }
+
+    @Test
+    public void getComposerName() {
+        assertEquals("Kenny G", sheet.getComposer());
     }
 }
 
