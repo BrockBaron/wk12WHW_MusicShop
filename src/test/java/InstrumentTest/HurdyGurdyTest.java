855beeeedfc23b5777d5c6342ba5225993721388
BrockBaron <Nslekkas@gmail.com>
2021-05-28 20:05:13 +0100

    HurdyGurdyTest setup strings tests needs defined

--- src/test/java/InstrumentTest/HurdyGurdyTest.java
+++ src/test/java/InstrumentTest/HurdyGurdyTest.java
@@ -2,6 +2,9 @@ package InstrumentTest;
 
 import Instruments.HurdyGurdy;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class HurdyGurdyTest {
 
@@ -10,11 +13,64 @@ public class HurdyGurdyTest {
 
     @Before
     public void setUp(){
-        hurdyGurdy = new HurdyGurdy("The 'Moncur'", "Chordophone", "Bowed", "Composite", "Wood", "Gutral-tones", 321.322, 730.54, 1987.68, 6);
+        hurdyGurdy = new HurdyGurdy("The 'Moncur'", "Chordophone", "Composite", "Bowed", "Wood", "Gutral-tones", 321.322, 730.54, 1987.68, 6);
     }
 
 
+    @Test
+    public void getName() {
+        assertEquals("The 'Moncur'", hurdyGurdy.getInstrumentName());
+    }
 
+    @Test
+    public void getInstrumentType() {
+        assertEquals("Chordophone", hurdyGurdy.getInstrumentType());
+    }
+
+    @Test
+    public void getInstrumentSubType() {
+        assertEquals("Composite", hurdyGurdy.getInstrumentSubType());
+    }
+
+    @Test
+    public void getInstrumentPlayMethod() {
+        assertEquals("Bowed", hurdyGurdy.getInstrumentPlayMethod());
+    }
+
+    @Test
+    public void getInstrumentBaseMaterial() {
+        assertEquals("Wood", hurdyGurdy.getInstrumentBaseMaterial());
+    }
+
+    @Test
+    public void getInstrumentSound() {
+        assertEquals("Gutral-tones", hurdyGurdy.getInstrumentSound());
+    }
+
+    @Test
+    public void canPlayInstrument() {
+        assertEquals("Gutral-tones", hurdyGurdy.playSound());
+    }
+
+    @Test
+    public void getHornSachId() {
+        assertEquals(321.322, hurdyGurdy.getHornSachId(), 0.1);
+    }
+
+    @Test
+    public void getWholesalePrice() {
+        assertEquals(730.54, hurdyGurdy.getWholesalePrice(), 0.1);
+    }
+
+    @Test
+    public void getRetailPrice() {
+        assertEquals(1987.68, hurdyGurdy.getRetailPrice(), 0.1);
+    }
+
+    @Test
+    public void getNoOfStrings() {
+        assertEquals(6, hurdyGurdy.getNoOfStrings());
+    }
 
 
 
