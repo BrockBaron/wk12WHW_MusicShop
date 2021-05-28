c08c27592b12e7289e13a3d27bfade777968bcab
BrockBaron <Nslekkas@gmail.com>
2021-05-28 20:01:57 +0100

    HurdyGurdyTest setup

--- src/test/java/InstrumentTest/HurdyGurdyTest.java
+++ src/test/java/InstrumentTest/HurdyGurdyTest.java
@@ -1,4 +1,41 @@
 package InstrumentTest;
 
+import Instruments.HurdyGurdy;
+import org.junit.Before;
+
 public class HurdyGurdyTest {
+
+    HurdyGurdy hurdyGurdy;
+
+
+    @Before
+    public void setUp(){
+        hurdyGurdy = new HurdyGurdy("The 'Moncur'", "Chordophone", "Bowed", "Composite", "Wood", "Gutral-tones", 321.322, 730.54, 1987.68, 6);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
