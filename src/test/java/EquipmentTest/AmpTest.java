e895008ce09704810083fcbf035479123bd36362
BrockBaron <Nslekkas@gmail.com>
2021-05-28 20:27:53 +0100

    Amp test started

--- src/test/java/EquipmentTest/AmpTest.java
+++ src/test/java/EquipmentTest/AmpTest.java
@@ -1,4 +1,16 @@
 package EquipmentTest;
 
+import Equipment.Amp;
+import org.junit.Before;
+
 public class AmpTest {
+
+    Amp amp;
+
+    @Before
+    public void setUp() {
+        amp = new Amp("MA9000", "McIntosh", "Amplifier", "Integrated Amplifier",7890.65, 12995.99,300);
+    }
+
+
 }
