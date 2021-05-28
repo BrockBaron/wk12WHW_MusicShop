b065613ddeff6013e256b16cf598c50e4f13b7cc
BrockBaron <Nslekkas@gmail.com>
2021-05-28 21:07:13 +0100

    Laser test completed

--- src/test/java/EquipmentTest/LaserTest.java
+++ src/test/java/EquipmentTest/LaserTest.java
@@ -2,6 +2,9 @@ package EquipmentTest;
 
 import Equipment.Laser;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class LaserTest {
 
@@ -12,5 +15,47 @@ public class LaserTest {
         laser = new Laser("CS-2000RGB", "Laserworld", "Laser", "FX Show Laser", 250.87, 1098.73, "RGB", 2500);
     }
 
+    @Test
+    public void hasName() {
+        assertEquals("CS-2000RGB", laser.getEquipmentName());
+    }
 
+    @Test
+    public void hasBrand() {
+        assertEquals("Laserworld", laser.getEquipmentBrand());
+    }
+
+    @Test
+    public void hasType() {
+        assertEquals("Laser", laser.getEquipmentType());
+    }
+
+    @Test
+    public void hasSubtype() {
+        assertEquals("FX Show Laser", laser.getEquipmentSubType());
+    }
+
+
+    @Test
+    public void getWholesalePrice() {
+        assertEquals(250.87, laser.getWholesalePrice(), 0.1);
+    }
+
+
+    @Test
+    public void getRetailPrice() {
+        assertEquals(1098.73, laser.getRetailPrice(), 0.1);
+
+    }
+
+    @Test
+    public void getLaserColours() {
+        assertEquals("RGB", laser.getLaserColour());
+    }
+
+    @Test
+    public void hasPowerValue() {
+        assertEquals(2500, laser.getLaserPower());
+
+    }
 }
