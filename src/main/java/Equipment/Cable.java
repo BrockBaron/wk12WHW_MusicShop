ce7eb47c85cc6884d22f6c295e315cbdab05ca69
BrockBaron <Nslekkas@gmail.com>
2021-05-28 20:42:54 +0100

    Cable test setup started

--- src/main/java/Equipment/Cable.java
+++ src/main/java/Equipment/Cable.java
@@ -3,13 +3,19 @@ package Equipment;
 public class Cable extends Equipment{
 
     public int wireGauge;
+    public double wireLenght;
 
-    public Cable(String equipmentName, String equipmentBrand, String equipmentType, String equipmentSubType, double wholesalePrice, double retailPrice, int wireGauge) {
+    public Cable(String equipmentName, String equipmentBrand, String equipmentType, String equipmentSubType, double wholesalePrice, double retailPrice, int wireGauge, double wireLenght) {
         super(equipmentName, equipmentBrand, equipmentType, equipmentSubType, wholesalePrice, retailPrice);
         this.wireGauge = wireGauge;
+        this.wireLenght = wireLenght;
     }
 
     public int getWireGauge() {
         return wireGauge;
+    }
+
+    public double getWireLenght() {
+        return wireLenght;
     }
 }
--- src/test/java/EquipmentTest/CableTest.java
+++ src/test/java/EquipmentTest/CableTest.java
@@ -1,4 +1,19 @@
 package EquipmentTest;
 
+import Equipment.Cable;
+import org.junit.Before;
+import org.junit.Test;
+
 public class CableTest {
+
+    Cable cable;
+
+    @Before
+    public void setUp(){
+        cable = new Cable("Dragon BiWire", "AudioQuest", "Cable", "Speaker cable", 150.50, 66800.99, 10, 4);
+    }
+
+    @Test
+    public void name() {
+    }
 }
